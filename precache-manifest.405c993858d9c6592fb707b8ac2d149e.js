c981ed5c88d3d5c1c8dfdb2b0e39a55cc5b9576b
Amanda <amanda.haynes@versett.com>
2020-05-27 09:19:25 -0600

    Updates

--- precache-manifest.405c993858d9c6592fb707b8ac2d149e.js
+++ precache-manifest.405c993858d9c6592fb707b8ac2d149e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "08339ffe9ce7c8cf775b58dd7755e631",
+    "revision": "b5884e0803200cf374c5e78eddc7d980",
     "url": "/reddit-offline/index.html"
   },
   {
-    "revision": "d925a05b4073a38b110e",
+    "revision": "7a19193cce01e5626d87",
     "url": "/reddit-offline/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "b6bc5249c1cb9caec8bc",
-    "url": "/reddit-offline/static/js/2.52683dcf.chunk.js"
+    "revision": "6540532bf233f048982f",
+    "url": "/reddit-offline/static/js/2.d4ec1651.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/reddit-offline/static/js/2.52683dcf.chunk.js.LICENSE.txt"
+    "url": "/reddit-offline/static/js/2.d4ec1651.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d925a05b4073a38b110e",
-    "url": "/reddit-offline/static/js/main.695ea3c5.chunk.js"
+    "revision": "7a19193cce01e5626d87",
+    "url": "/reddit-offline/static/js/main.6b90cea4.chunk.js"
   },
   {
     "revision": "beb65459669fb9428170",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reddit-offline/precache-manifest.da0f5e8f3e02f730b5e693a3fc3dfd98.js"
+  "/reddit-offline/precache-manifest.405c993858d9c6592fb707b8ac2d149e.js"
 );
 
 self.addEventListener('message', (event) => {
