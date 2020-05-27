0199caeb51bebcfa49d84875cefac2d91b5cf2bb
Amanda <amanda.haynes@versett.com>
2020-05-27 07:44:44 -0600

    Updates

---
+++ precache-manifest.2c907ab084d86a463b08eea6fc8f96f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb6f1686891dfc6e0845db379c450d8",
+    "url": "/reddit-offline/index.html"
+  },
+  {
+    "revision": "c88a9269115b78c407fc",
+    "url": "/reddit-offline/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9883e1930b6ed611b4ce",
+    "url": "/reddit-offline/static/js/2.d5919b3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reddit-offline/static/js/2.d5919b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c88a9269115b78c407fc",
+    "url": "/reddit-offline/static/js/main.528c894d.chunk.js"
+  },
+  {
+    "revision": "ed6b3a2a3f360d47968e",
+    "url": "/reddit-offline/static/js/runtime-main.d41c319c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reddit-offline/static/media/logo.5d5d9eef.svg"
+  }
+]);
