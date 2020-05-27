caf20def514abcfc40dd68fcb4016ec998c1a18c
Amanda <amanda.haynes@versett.com>
2020-05-27 09:12:36 -0600

    Updates

--- precache-manifest.e5a3a37972f52c9fc06e5f8b2549ebb8.js
+++ precache-manifest.e5a3a37972f52c9fc06e5f8b2549ebb8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "96d0eee2ab5d0f47b3f9e35adcec3547",
+    "revision": "ecd435aac3f296ad883b97376a62cf4e",
     "url": "/reddit-offline/index.html"
   },
   {
-    "revision": "3776bc5bf0417fc79396",
+    "revision": "a2f8eee1e2b7eb869f68",
     "url": "/reddit-offline/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reddit-offline/static/js/2.52683dcf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3776bc5bf0417fc79396",
-    "url": "/reddit-offline/static/js/main.b4db9756.chunk.js"
+    "revision": "a2f8eee1e2b7eb869f68",
+    "url": "/reddit-offline/static/js/main.eb58b6aa.chunk.js"
   },
   {
     "revision": "beb65459669fb9428170",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reddit-offline/precache-manifest.87688587b951249e986720353177aef3.js"
+  "/reddit-offline/precache-manifest.e5a3a37972f52c9fc06e5f8b2549ebb8.js"
 );
 
 self.addEventListener('message', (event) => {
